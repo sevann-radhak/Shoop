d38ffb340ace8a3cc30a7f8a5d851950d267228c
Sevann Radhak <sevann.radhak@gmail.com>
2019-11-03 16:10:12 -0300

    Create the project

---
+++ Shoop.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shoop.UIClassic
+{
+    public class Class1
+    {
+    }
+}
---
+++ Shoop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shoop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
