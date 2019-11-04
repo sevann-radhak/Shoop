53a395bd06ee4afd2d988ee2dd3d68145abc9abf
Sevann Radhak <sevann.radhak@gmail.com>
2019-11-04 00:27:13 -0300

    First API: ProductsController: api/products

---
+++ Shoop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shoop.Web.Data;
+using Shoop.Web.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shoop.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+        //private readonly IUserHelper userHelper;
+
+        //public ProductsController(IProductRepository productRepository, IUserHelper userHelper)
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+            //this.userHelper = userHelper;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
